c33b4cd77e8db74dbe61d6b24a22884e3d463c69
Widhi Wadher <widhiwadher9@gmail.com>
2021-06-17 16:35:49 +0530

    login based on role

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
 const express = require("express")
- const app = express();
- const port = 8080;
- const hbs = require("hbs");
- const path = require("path");
- const partialsPath = path.join(__dirname, '/views/partials');
+const app = express();
+const port = 8080;
+const hbs = require("hbs");
+const path = require("path");
+const partialsPath = path.join(__dirname, '/views/partials');
+const users = require("./users");
 
- app.set ('view engine', 'hbs');
- hbs.registerPartials(partialsPath);
+app.set('view engine', 'hbs');
+hbs.registerPartials(partialsPath);
 
- app.get("/", (req,res) => {
+const json = require("./users.json")
+console.log(json);
+
+app.get("/", (req, res) => {
     res.render('index');
- })
+})
 
- app.get("/", (req,res)=> {
- res.send("hello world- explore my thoughts");
- })
+app.get("/login", (req, res) => {
+    res.render('login');
+})
+
+app.get("/", (req, res) => {
+    res.send("hello world- explore my thoughts");
+})
+
+ app.post ("/submit", (req,res) => {
+    for (i = 0; i < json.length; i++) {
+        var items = json[i];
+       console.log(item);
+    }   
+ });
 
 
- app.listen(port, (req,res) => {
-     console.log("listening at 8080");
- })
+
+app.listen(port, (req, res) => {
+    console.log("listening at 8080");
+})
+
 
